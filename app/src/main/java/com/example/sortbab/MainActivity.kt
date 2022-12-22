e51cd306c8b6add947172dd37de87ff3c069e1fa
Jamie <ishighjamie@gmail.com>
2022-12-22 19:10:48 +0100

    Deklaracja

--- app/src/main/java/com/example/sortbab/MainActivity.kt
+++ app/src/main/java/com/example/sortbab/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.sortbab
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
 import android.widget.TextView
 import kotlin.random.Random
 
@@ -11,6 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val los = findViewById<Button>(R.id.losuj);
+        val sor = findViewById<Button>(R.id.sortuj);
+        val text = findViewById<TextView>(R.id.textView);
 
+        var list1 = arrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0)
+        var list2: Array<Switch> = arrayOf(findViewById(R.id.s1), findViewById(R.id.s2), findViewById(R.id.s3), findViewById(R.id.s4), findViewById(R.id.s5), findViewById(R.id.s6), findViewById(R.id.s7), findViewById(R.id.s8), findViewById(R.id.s9),)
     }
 }
